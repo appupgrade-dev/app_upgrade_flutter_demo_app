45d89d553e6e8287d65134a398315aca5b9fb0e5
App Upgrade <appupgrade.dev@gmail.com>
2023-01-02 20:56:05 +0530

    Add readme

--- README.md
+++ README.md
@@ -14,3 +14,66 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+# App Upgrade: Flutter Demo App
+
+[App Upgrade: Flutter Demo App](https://github.com/appupgrade-dev/app_upgrade_flutter_demo_app) is a sample flutter app integrated with [App Upgrade](https://appupgrade.dev) via API to demonstrate how Force upgrade works in flutter app with [App Upgrade](https://appupgrade.dev) API.
+
+You can find the demo app integrated via SDK in [main](https://github.com/appupgrade-dev/app-upgrade-assets) branch.
+
+## Installation
+
+To get started, clone the repo:
+
+`$ git clone https://github.com/appupgrade-dev/app_upgrade_flutter_demo_app`
+
+## Setup
+
+1. Once cloned, navigate to the `app_upgrade_flutter_demo_app` directory.
+
+   `$ cd app_upgrade_flutter_demo_app`
+
+2. Open the directory in your code editor.
+
+3. Open the `app_upgrade_flutter_demo_app/lib/main.dart` file, and replace required values in line 81-86
+   | Key                   | Value Description |
+   | -----------------------|-------------|
+   | `x-api-key`     | Your API Key. Required. |
+   | `appName`  | Your app name. Required. |
+   | `appVersion`  | Your app version. Required. |
+   | `platform`  | Your app platform, ex: android or iOS. Required. |
+   | `environment`  | Your app enviroment, ex: dev, stating, production. Required. |
+
+   Example:
+
+   ```js
+      Uri.parse(
+          'https://appupgrade.dev/api/v1/versions/check?app_name=Wallpaper app&app_version=1.0.0&platform=android&environment=production'),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "x-api-key": "ZWY0ZDhjYjgtYThmMC00NTg5LWI0NmUtMjM5OWZkNjkzMzQ5"
+      },
+   ```
+
+4. Save `main.dart` file.
+
+## Run the app:
+   Start the app
+   `$ flutter run --no-sound-null-safety`
+
+## Usage:   
+
+1. Head to [App Upgrade](https://appupgrade.dev) and signup if not already.
+2. Create a new Project.
+3. Grab the API Key. You will need to provide the same in the required fields mentioned in above section.
+4. Create a new version with the required details and force upgrade field as selected. Make sure the details provided in version matches with the details provided in the App.js file.
+5. Refresh the app and you will be able to see a popup for force upgrade.
+6. Edit the version in dashboard with Force upgrade field unselected. Refresh the app and you will see a popup with soft upgrade with a button to upgrade later.
+ > For full details on how to use App Upgrade dashboard read our [Documentation](https://appupgrade.dev/docs)
+
+## Screenshots
+ ![forceupgrade_flutter_sdk](https://raw.githubusercontent.com/appupgrade-dev/app-upgrade-assets/main/images/forceupgrade_flutter_sdk.png)
+
+## Need help?
+
+If you're looking for help, try our [Documentation](https://appupgrade.dev/docs/) or our [FAQ](https://appupgrade.dev/docs/app-upgrade-faq).
+If you need support please write to us at appupgrade.dev@gmail.com
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:http/http.dart' as http;
-
-import 'models/version-check.dart';
+import 'package:app_upgrade_flutter_sdk/app_upgrade_flutter_sdk.dart';
 
 void main() {
   runApp(const MyApp());
-  HttpOverrides.global = MyHttpOverrides();
-}
-
-class MyHttpOverrides extends HttpOverrides{
-  @override
-  HttpClient createHttpClient(SecurityContext? context){
-    return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -26,204 +11,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    AppInfo appInfo = AppInfo(
+      appName: 'Wallpaper app', // Your app name
+      appVersion: '1.0.0', // Your app version
+      platform: 'android', // App Platform, android or ios
+      environment:
+          'production', // Environment in which app is running, production, staging or development etc.
+    );
+
     return MaterialApp(
-      title: 'App Upgrade Demo App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primaryColor: const Color(0XFFea1e5b),
-      ),
-      home: const MyHomePage(title: 'App Upgrade Demo App'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  int upgradeRequired = 0;
-  int forceUpgradeRequired = 0;
-  String upgradeMessage = '';
-
-  initState() {
-    print('check for upgrade');
-    _checkForUpgrade();
-    super.initState();
-  }
-
-  Future<void> _checkForUpgrade() async {
-    final response = await http.get(
-      Uri.parse(
-          'https://appupgrade.dev/api/v1/versions/check?app_name=Wallpaper app&app_version=1.0.0&platform=android&environment=production'),
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        "x-api-key": "ZjdiMDFmOWUtNTM5Ni00OTNhLWI1NjUtMjNiODc1NmJiNjEx"
-      },
-    );
-
-    if (response.statusCode == 200) {
-      var data = VersionCheck.fromJson(jsonDecode(response.body));
-      print(data.found);
-      if (data.found == true) {
-        if (data.forceUpgrade == true) {
-          setState(() {
-            forceUpgradeRequired = 1;
-            upgradeMessage = data.message;
-          });
-        } else {
-          setState(() {
-            upgradeRequired = 1;
-            upgradeMessage = data.message;
-          });
-        }
-      }
-    } else {
-      print(json.decode(response.body)['message']);
-      throw Exception('Failed to call api');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Stack(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          children: <Widget>[
-            Container(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    child: ListView.builder(
-                        // scrollDirection: Axis.horizontal,
-                        itemCount: 5,
-                        itemBuilder: (context, index) {
-                          var id = 1000 + index;
-                          var imgUrl = 'https://picsum.photos/id/' + id.toString() + '/640/480';
-                          return Container(
-                            padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                            height: 220,
-                            width: double.maxFinite,
-                            child: Card(
-                              elevation: 5,
-                              semanticContainer: true,
-                              clipBehavior: Clip.antiAliasWithSaveLayer,
-                              child: Image.network(
-                                imgUrl,
-                                fit: BoxFit.fill,
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                            ),
-                          );
-                        }),
-                  ),
-                ],
-              ),
-            ),
-            (forceUpgradeRequired == 1)
-                ? AlertDialog(
-                    title: const Text('Please update the app'),
-                    content: SingleChildScrollView(
-                      child: ListBody(
-                        children: <Widget>[
-                          Text(upgradeMessage),
-                        ],
-                      ),
-                    ),
-                    actions: <Widget>[
-                      TextButton(
-                        child: const Text('Update'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ],
-                  )
-                : Container(),
-            (upgradeRequired == 1)
-                ? AlertDialog(
-                    title: const Text('Please update the app'),
-                    content: SingleChildScrollView(
-                      child: ListBody(
-                        children: <Widget>[
-                          Text(upgradeMessage),
-                        ],
-                      ),
-                    ),
-                    actions: <Widget>[
-                      TextButton(
-                        child: const Text('Later'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      TextButton(
-                        child: const Text('Update'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ],
-                  )
-                : Container(),
-          ],
-        ),
-      ),
+      title: 'Upgrader Example',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('App Upgrade Flutter Demo App'),
+          ),
+          body: AppUpgradeAlert(
+            xApiKey:
+                'ZWY0ZDhjYjgtYThmMC00NTg5LWI0NmUtMjM5OWZkNjkzMzQ5', // Your x-api-key
+            appInfo: appInfo,
+            child: Center(child: Text('Hello World!')),
+          )),
     );
   }
 }
